ca38bf27612a53782781e2a46751e0396e947185
PatilKavya <kavyapatilgonalst@gmail.com>
2023-03-30 15:14:25 +0530

    req.url

--- first.js
+++ first.js
@@ -1,1 +1,25 @@
-console.log('hello World!')
+// console.log('hello World!')
+
+const http=require('http');
+
+http.createServer((req,res)=>{
+    console.log(req.headers,req.method,req.url);
+    res.setHeader('Content-Type','text/html');
+    if(req.url==='/home'){
+        res.write('<html>')
+        res.write('<body> <h1>Hello home! </h1></body>');
+        res.write('</html>')
+    }
+    else if(req.url==='/about'){
+        res.write('<html>')
+        res.write('<body> <h1>About </h1></body>');
+        res.write('</html>')
+    }
+    if(req.url==='/node'){
+        res.write('<html>')
+        res.write('<body> <h1>welcome to node.js </h1></body>');
+        res.write('</html>')
+    }
+    res.end()
+   // process.exit();
+}).listen(3000)
