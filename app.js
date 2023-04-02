f248dd896eca6fe591acbac273d0ed6306186216
PatilKavya <kavyapatilgonalst@gmail.com>
2023-04-02 15:25:07 +0530

    bodyParser

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 //const http=require('http');
 
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use("/add-product", (req,res,next)=>{
 console.log('in the middle Ware');
-//res.send('<h1>Hello World!</h1>');
+res.send("<form action='/product' method='POST'><input type='text' name='title'/><input type='number' name='quantity'/><button type='submit'>Add Product</button> </form>");
 next();
 })
 
-app.use((req,res,next)=>{
+app.use("/product", (req,res,next)=>{
+    console.log('in the middle Ware');
+console.log(req.body)
+    res.redirect('/')
+    })
+
+app.use("/",(req,res,next)=>{
     console.log('in the other middle Ware');
    res.send('<h1>Hello next!</h1>');
    // next();
