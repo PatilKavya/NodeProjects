a39f00ff0cf8f5721adf861386bbf7e40d25f0fb
PatilKavya <kavyapatilgonalst@gmail.com>
2023-04-02 22:30:10 +0530

    html,css

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 //const http=require('http');
-
+const path=require('path')
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// const adminRouter=require('./routes/admin');
-// const shopRouter=require('./routes/shop');
-const loginRouter=require('./routes/login');
-const messageRouter=require('./routes/message')
+const adminRouter=require('./routes/admin');
+const shopRouter=require('./routes/shop');
+const contactRoutr=require('./routes/contact')
+// const loginRouter=require('./routes/login');
+// const messageRouter=require('./routes/message')
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')))
 
-// app.use('/admin',adminRouter);
-// app.use(shopRouter)
+app.use(adminRouter);
+app.use(shopRouter);
+app.use(contactRoutr);
 
-app.use(loginRouter);
+// app.use(loginRouter);
 
-app.use(messageRouter)
-
+// app.use(messageRouter)
+app.use('/success',(req,res,next)=>{
+    res.send('<h1>Form successfuly filled</h1>')
+}
+)
 app.use((req,res)=>{
-res.status(404).send("<h2>Page Not Found</h2>")
+res.status(404).sendFile(path.join(__dirname,'./','views','nopage.html'))
 })
 // const server=http.createServer(app);
 
